5260f9a81218baa342b1e027aef5fd73372d981e
Pradeep <shanpradeep2023@gmail.com>
2026-06-21 20:30:36 +0530

    Initial Commit

---
+++ slms-backend/src/main/java/com/pradeep/slms/SlmsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.pradeep.slms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlmsBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SlmsBackendApplication.class, args);
+    }
+
+}
---
+++ slms-backend/src/test/java/com/pradeep/slms/SlmsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pradeep.slms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlmsBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
